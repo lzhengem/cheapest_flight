bec2cad87e2cb31c70b061008f1f78e3198e1a98
Lena <lzhengem@gmail.com>
2019-02-01 08:20:06 -0800

    feat: able to render the template homepage.html when visiting /

--- project.py
+++ project.py
@@ -1,16 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 import json
+import requests
+from amadeus import Client, ResponseError
 
 app = Flask(__name__)
 
 AMADEUS_ID = json.loads(open('amadeus_client_secrets.json','r').read())['amadeus']['client_id']
 AMADEUS_SECRET = json.loads(open('amadeus_client_secrets.json','r').read())['amadeus']['client_secret']
 
+amadeus = Client(
+    client_id=AMADEUS_ID,
+    client_secret=AMADEUS_SECRET
+)
+
 
 @app.route('/')
 def home():
-    return "Welcome to the homepage"
-
+    return render_template("homepage.html")
+  
 if __name__ == '__main__':
+    app.debug = True    
     app.run(host='0.0.0.0', port = 8500)
-    app.debug = true
