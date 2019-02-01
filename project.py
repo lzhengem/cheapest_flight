ff4d04984fe08e491198b140d9ab90ac8a2f5b2a
Lena <lzhengem@gmail.com>
2019-01-31 19:24:26 -0800

    feat: added homepage

--- project.py
+++ project.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 AMADEUS_ID = json.loads(open('amadeus_client_secrets.json','r').read())['amadeus']['client_id']
 AMADEUS_SECRET = json.loads(open('amadeus_client_secrets.json','r').read())['amadeus']['client_secret']
 
+
+@app.route('/')
+def home():
+    return "Welcome to the homepage"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 8500)
+    app.debug = true
